ccdc1eb9de3f712baef0e3b561171dcb93eb9560
anastasia <anastasia@emea-m-5241.speedport.ip>
2022-12-23 13:35:55 +0100

    completed task 2 in js folder

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.getElementById('ingredients');
+console.log(list);
+
+for (const ingredient of ingredients) {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  list.append(li);
+}
+
+document.body.append(list);
