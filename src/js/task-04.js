9d5859157bde6d0b02a92a5c6969e2fabe7d2707
anastasia <anastasia@emea-m-5241.speedport.ip>
2022-12-25 16:22:38 +0100

    completed task 4

---
+++ src/js/task-04.js
@@ -0,0 +1,23 @@
+let counterValue = 0;
+const counterValueEl = document.querySelector('#value');
+
+const decrementBtn = document.querySelector('button[data-action="decrement"');
+const incrementBtn = document.querySelector('button[data-action="increment"');
+
+decrementBtn.addEventListener('click', onDecrementBtnClick);
+
+incrementBtn.addEventListener('click', onIncrementBtnClick);
+
+function onDecrementBtnClick() {
+    counterValue -= 1;
+    updateCounterValueEl(counterValue);
+};
+
+function onIncrementBtnClick() {
+    counterValue += 1;
+    updateCounterValueEl(counterValue);
+};
+
+function updateCounterValueEl(val) {
+     counterValueEl.textContent = val;
+}
