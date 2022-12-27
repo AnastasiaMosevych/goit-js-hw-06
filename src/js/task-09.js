f61a5282b3056fccb7cb379a306906a573b2a3e8
anastasia <anastasia@emea-m-5241.speedport.ip>
2022-12-27 18:55:52 +0100

    completed task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,22 @@
+// Напиши скрипт, який змінює кольори фону елемента < body > через інлайн - стиль 
+// по кліку на button.change - color і виводить значення кольору в span.color.
+// 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  body.style.backgroundColor = color;
+}
+
+
